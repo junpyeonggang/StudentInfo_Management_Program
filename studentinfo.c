f8a669008806c4806c31116f604e34788a4e38c0
junpyeonggang <w1218is@naver.com>
2022-12-12 15:30:16 +0900

    insert

--- studentinfo.c
+++ studentinfo.c
@@ -16,7 +16,7 @@ int x, y;
 typedef struct student { 
 
  	char name[15];	
-	char major[25];	
+	char major[25];	 
 	char age[10];	
 	char phone_number[25];	
 	struct student* next;
@@ -44,6 +44,26 @@ void cursor_move(int q)
 	cursor_toxy(3, y);
 
 	printf("▶");		
+}
+
+
+student* insert(void)	
+{
+	student* a;
+	a = (student*)malloc(sizeof(student));
+	cursor_toxy(22, 5); printf("   이름 : ");
+	cursor_toxy(32, 5); fgets(a->name, 15, stdin);
+	cursor_toxy(22, 7); printf("   전공 : ");
+	cursor_toxy(32, 7); fgets(a->major, 25, stdin);
+	cursor_toxy(22, 9); printf("   나이 : ");
+	cursor_toxy(32, 9); fgets(a->age, 10, stdin);
+	cursor_toxy(22, 11);	printf("   전화번호 : ");
+	cursor_toxy(36, 11); fgets(a->phone_number, 25, stdin);
+
+	a -> next = head -> next;
+	head -> next = a;
+
+	return a;
 }
 
 void cursor_toxy(int x, int y)	
