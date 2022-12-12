502bad9b2ed7b9dab2caea7458e97af2e281f73b
junpyeonggang <w1218is@naver.com>
2022-12-12 14:49:05 +0900

    struct linked list

--- studentinfo.c
+++ studentinfo.c
@@ -3,3 +3,25 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
+
+#define INSERT 5
+#define PRINT 7
+#define SEARCH 9
+#define UPDATE 11
+#define SAVE 13
+#define EXIT 15
+
+int x, y;		
+
+typedef struct student { 
+
+ 	char name[15];	
+	char major[25];	
+	char age[10];	
+	char phone_number[25];	
+	struct student* next;
+
+}student;
+
+student* head, * tail;		
+
